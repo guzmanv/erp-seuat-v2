451d16fe01c2b2d2f0363d000ceb57f9fa27d2b4
JOSEPH1990a <JOSE_SANTIZR1990@HOTMAIL.COM>
2021-11-06 14:07:43 -0600

    modificando el archivo de Vista plantel.php

--- Views/Plantel/plantel.php
+++ Views/Plantel/plantel.php
@@ -1,157 +1,62 @@
 <?php
-  headerAdmin($data);
+    headerAdmin($data);
+    getModal('Plantel/modalNuevoPlantel',$data);
+    getModal('Plantel/modalVerPlantel',$data);
+    getModal('Plantel/modalEditPlantel',$data);
 ?>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">  Administración de planteles</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Plantel</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <div class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-lg-12">
-
-
-            <div class="card">
-              <div class="card-body">
-                <h3 class="card-title">Listado de planteles</h3>
-                <p class="card-text">
-              <!-- /.card-header -->
-                <table id="tableRoles" class="table table-bordered table-striped table-sm">
-                  <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Descripción</th>
-                    <th>Estatus</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-
-                  </tbody>
-
-                </table>
-              </p>
-              </div>
-              <!-- /.card-body -->
+<div id="contentAjax"></div>
+<div class="wrapper">
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-7">
+                        <h1 class="m-0">  <?= $data['page_title'] ?></h1>
+                    </div>
+                    <div class="col-sm-5">
+                        <ol class="breadcrumb float-sm-right btn-block">
+                            <button type="button"  class="btn btn-inline btn-primary btn-sm btn-block" data-toggle="modal" data-target="#ModalFormNuevoPlantel"><i class="fa fa-plus-circle fa-md"></i> Nuevo</button>
+                            <!--<li class="breadcrumb-item"><i class="fa fa-home fa-md"></i><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active"><a href="<?= base_url(); ?>/roles"><?= $data['page_title'] ?></a></li>-->
+                        </ol>
+                    </div>
+                </div>
             </div>
-
-
-<!--
-            <div class="card shadow-sm p-3 mb-5 bg-white rounded">
-              <div class="card-body">
-                <h5 class="card-title">Planteles</h5>
-
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-
-                <a href="#" class="card-link">link uno</a>
-                <a href="#" class="card-link">link dos</a>
-              </div>
-            </div>-->
-
-
-          <!-- /.card -->
-          </div>
-          <!-- /.col-md-6 -->
-       <!--   <div class="col-lg-12">
-            <div class="card">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>-->
-          <!-- /.col-md-6 -->
         </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+<!--                                 <h3 class="card-title">Listado de planteles</h3>
+ -->                                <p class="card-text">
+                                    <table id="tablePlantel" class="table table-bordered table-striped table-sm">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Plantel</th>
+                                                <th>Estado</th>
+                                                <th>Municipio</th>
+                                                <th>Localidad</th>
+                                                <th>Regimen</th>
+                                                <th>Servicio</th>
+                                                <th>Acciones</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                        </tbody>
+                                    </table>
+                                </p>
+                            </div>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
-  <!-- Control Sidebar -->
-
-  <!--<aside class="control-sidebar control-sidebar-dark">
-     Control sidebar content goes here -->
-    <!--<div class="p-3">
-      <h5>Title</h5>
-      <p>Sidebar content</p>
-    </div>
-  </aside>-->
-  <!-- /.control-sidebar -->
-
-
-
+</div>
 <?php footerAdmin($data); ?>
-
-
-
-<!-- Page specific script -->
-<script>
-  //$(function () {
-  $(document).ready(function() {
-    $("#tableRoles").DataTable({
-      "responsive": true,
-      "paging": true,
-      "lengthChange": true,
-      "searching": true,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
-      "scrollY": '42vh',
-      "scrollCollapse": true,
-      "bDestroy": true,
-      "order": [[ 0, "desc" ]],
-      "iDisplayLength": 25,
-      language: {
-            url:"<?php echo media(); ?>/plugins/Spanish.json",
-        }
-
-    });
-
-
-
-
-   /* $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
-      "responsive": true,
-      language: {
-            url: '<?php echo media(); ?>/plugins/Spanish.json'
-        }
-    });*/
-  });
-
-</script>
 
 
