a62a9b6bff7a9df2e669edeba8d926f426966548
Emmanuel <eespinest87@gmail.com>
2021-11-27 16:00:13 -0600

    Reconfiguración vista modelo y controlador salones

--- Controllers/Salones.php
+++ Controllers/Salones.php
@@ -9,11 +9,6 @@ class Salones extends Controllers{
         $data['page_tag'] = 'Salones';
         $data['page_title'] = 'Salones';
         $data['data_name'] = 'Salones';
-        $data['periodo'] = $this->model->selectPeriodo();
-        $data['grado'] = $this->model->selectGrado();
-        $data['grupo'] = $this->model->selectGrupo();
-        $data['turnos'] = $this->model->selectTurnos();
-        $data['planteles'] = $this->model->selectPlanteles();
         $data['page_functions_js'] = 'functions_salones.js';
         $this->views->getView($this,'salon',$data);
     }
@@ -23,14 +18,6 @@ class Salones extends Controllers{
         $arrData = $this->model->selectSalones();
         for($i=0; $i<count($arrData); $i++)
         {
-            // if($arrData[$i]['estatus'] ==1)
-            // {
-            //     $arrData[$i]['estatus'] = '<span class="badge badge-dark">Activo</span>';
-            // }
-            // else
-            // {
-            //     $arrData[$i]['estatus'] = '<span class="badge badge-secondary">Inactivo</span>';
-            // }
             $arrData[$i]['options'] = '<div class="text-center">
             <div class="btn-group">
                 <button type="" class="btn btn-outline-secondary btn-xs icono-color-principal dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-layer-group"></i> &nbsp; Acciones
@@ -51,41 +38,41 @@ class Salones extends Controllers{
 
     public function setSalon()
     {
-        $data = $_POST;
-        $intIdSalonNuevo = 0;
-        $intIdSalonEdit = 0;
-        if(isset($_POST['idSalonNuevo'])){
-            $intIdSalonNuevo = intval($_POST['idSalonNuevo']);
-        }
-        if(isset($_POST['idEdit'])){
-            $intIdSalonEdit = intval($_POST['idEdit']);
-        }
+        // $data = $_POST;
+        // $intIdSalonNuevo = 0;
+        // $intIdSalonEdit = 0;
+        // if(isset($_POST['idSalonNuevo'])){
+        //     $intIdSalonNuevo = intval($_POST['idSalonNuevo']);
+        // }
+        // if(isset($_POST['idEdit'])){
+        //     $intIdSalonEdit = intval($_POST['idEdit']);
+        // }
 
-        if($intIdSalonNuevo == 1)
-        {
-            $arrData = $this->model->insertSalon($data);
-            if($arrData['estatus'] != TRUE)
-            {
-                $arrResponse = array('estatus' => true, 'msg' => 'Datos guardados correctamente');
-            }
-            else
-            {
-                $arrResponse = array('estatus' => false, 'msg' => '¡Atención! el salón ya existe');
-            }
-        }
-        if($intIdSalonEdit != 0){
-            $arrData = $this->model->updateSalon($intIdSalonEdit,$data);
-            if($arrData)
-            {
-                $arrResponse = array('estatus' => true, 'msg' => 'Datos actualizados correctamente');
-            }
-            else
-            {
-                $arrResponse = array('estatus' => false, 'msg' => 'No es posible actualizar los datos');
-            }
-        }
-        echo json_encode($arrResponse, JSON_UNESCAPED_UNICODE);
-        die();
+        // if($intIdSalonNuevo == 1)
+        // {
+        //     $arrData = $this->model->insertSalon($data);
+        //     if($arrData['estatus'] != TRUE)
+        //     {
+        //         $arrResponse = array('estatus' => true, 'msg' => 'Datos guardados correctamente');
+        //     }
+        //     else
+        //     {
+        //         $arrResponse = array('estatus' => false, 'msg' => '¡Atención! el salón ya existe');
+        //     }
+        // }
+        // if($intIdSalonEdit != 0){
+        //     $arrData = $this->model->updateSalon($intIdSalonEdit,$data);
+        //     if($arrData)
+        //     {
+        //         $arrResponse = array('estatus' => true, 'msg' => 'Datos actualizados correctamente');
+        //     }
+        //     else
+        //     {
+        //         $arrResponse = array('estatus' => false, 'msg' => 'No es posible actualizar los datos');
+        //     }
+        // }
+        // echo json_encode($arrResponse, JSON_UNESCAPED_UNICODE);
+        // die();
     }
 }
 ?>
--- Models/SalonesModel.php
+++ Models/SalonesModel.php
@@ -7,77 +7,14 @@
 
         public function selectSalones()
         {
-            $sql = "SELECT saln.id, saln.nombre_salon, saln.cantidadmax, saln.estatus, per.nombre_periodo , grad.nombre_grado , grup.nombre_grupo, plant.abreviacion_plantel
-            FROM t_salones as saln
-            INNER JOIN t_periodos as per
-            ON saln.id_periodo = per.id 
-            INNER JOIN t_grados as grad
-            ON saln.id_grado = grad.id
-            INNER JOIN t_grupos as grup 
-            ON saln.id_grupo = grup.id
-            INNER JOIN t_planteles as plant
-            ON saln.id_plantel = plant.id";
+            $sql = "SELECT id, nombre_salon, cantidad_max_estudiantes FROM t_salones";
             $request = $this->select_all($sql);
             return $request;
         }
 
-        public function selectPeriodo()
-        {
-            $sql = "SELECT * FROM t_periodos";
-            $request = $this->select_all($sql);
-            return $request;
-        }
-
-        public function selectGrado()
-        {
-            $sql = "SELECT * FROM t_grados";
-            $request = $this->select_all($sql);
-            return $request;
-        }
-
-        public function selectGrupo()
-        {
-            $sql = "SELECT * FROM t_grupos";
-            $request = $this->select_all($sql);
-            return $request;
-        }
-
-        public function selectTurnos()
-        {
-            $sql = "SELECT id,nombre_turno, TIME_FORMAT(hora_entrada, '%H:%i') as hora_entrada, TIME_FORMAT(hora_salida, '%H:%i') as hora_salida FROM t_turnos where id_categoria_persona = 2";
-            $request = $this->select_all($sql);
-            return $request;
-        }
-
-        public function selectPlanteles()
-        {
-            $sql = "SELECT * FROM t_planteles";
-            $request = $this->select_all($sql);
-            return $request;
-        }
 
         public function insertSalon($data){
-            $nombreSalon = $data['txtNombreNuevo'];
-            $cantidadMax = $data['txtCantidadMax'];
-            $periodo = $data['listPeriodo'];
-            $grado = $data['listGrado'];
-            $grupo = $data['listGrupo'];
-            $plantel = $data['listPlantel'];
-            $horario = $data['listHorario'];
-            $request;
-            $sqlExist = "SELECT * FROM t_salones where nombre_salon = '$nombreSalon' and id_periodo = $periodo and id_plantel=$plantel;";
-            $requestExists = $this->select($sqlExist);
-            if($requestExists)
-            {
-                $request['estatus'] = TRUE;
-            }
-            else{
-                $sqlNew = "INSERT INTO t_salones(nombre_salon, cantidadmax, estatus, id_periodo, id_grado, id_grupo, id_plantel, id_horario, fecha_creacion,id_usuario_creacion) VALUES(?,?,1,?,?,?,?,?,NOW(),1)";
-                $requestNew = $this->insert($sqlNew, array($nombreSalon, $cantidadMax, $periodo, $grado, $grupo, $plantel, $horario));
-                $request['estatus'] = FALSE;
-            }
-
-            return $request;
+            
         }
     }
 ?>
