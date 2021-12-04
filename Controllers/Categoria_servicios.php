1154d41a8d4824f95e9e5bcf4e21a1627d5a53ec
JOSEPH1990a <JOSE_SANTIZR1990@HOTMAIL.COM>
2021-12-04 14:50:10 -0600

    agregando promociones y categorias servicios en ingresos

---
+++ Controllers/Categoria_servicios.php
@@ -0,0 +1,189 @@
+<?php
+	class Categoria_servicios extends Controllers
+    {
+			public function __construct()
+			{
+				parent::__construct();
+			}
+
+
+			public function Categoria_servicios()
+			{
+							$data['page_tag'] = "Categoría de servicios";
+							$data['page_title'] = "Categoría servicios";
+							$data['page_name'] = "categoria_servicios";
+							$data['page_functions_js'] = "functions_categoria_servicios.js";
+							$this->views->getView($this,"categoria_servicios",$data);
+			}
+
+
+			public function getCategoria_servicios()
+			{
+				$arrData = $this->model->selectCategoria_servicios();
+
+				for ($i=0; $i < count($arrData); $i++) {
+
+					if($arrData[$i]['estatus'] == 1)
+					{
+						$arrData[$i]['estatus'] = '<span class="badge badge-dark">Activo</span>';
+					}else{
+						$arrData[$i]['estatus'] = '<span class="badge badge-secondary">Inactivo</span>';
+					}
+
+					$arrData[$i]['options'] = '
+					<div class="text-center">
+						<div class="btn-group">
+							<button type="button" class="btn btn-outline-secondary btn-xs icono-color-principal dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<i class="fas fa-layer-group"></i> &nbsp; Acciones
+							</button>
+							<div class="dropdown-menu">
+								<button class="dropdown-item btn btn-outline-secondary btn-sm btn-flat icono-color-principal btnEditCategoria_servicios" onClick="fntEditCategoria_servicios(this,'.$arrData[$i]['id'].')" title="Editar"> &nbsp;&nbsp; <i class="fas fa-pencil-alt"></i> &nbsp; Editar</button>
+								<div class="dropdown-divider"></div>
+								<button class="dropdown-item btn btn-outline-secondary btn-sm btn-flat icono-color-principal btnDelCategoria_servicios" onClick="fntDelCategoria_servicios('.$arrData[$i]['id'].')" title="Eliminar"> &nbsp;&nbsp; <i class="far fa-trash-alt "></i> &nbsp; Eliminar</button>
+							</div>
+						</div>
+					</div>';
+				}
+				echo json_encode($arrData,JSON_UNESCAPED_UNICODE);
+				die();
+			}
+			
+
+			public function getCategoria_servicio($id){
+				//if($_SESSION['permisosMod']['r']){
+					$intIdCategoria_servicios = intval(strClean($id)); //intval(strClean($idrol));
+					if($intIdCategoria_servicios > 0)
+					{
+						$arrData = $this->model->selectCategoria_servicio($intIdCategoria_servicios);
+						if(empty($arrData))
+						{
+							$arrResponse = array('estatus' => false, 'msg' => 'Datos no encontrados.');
+						}else{
+							$arrResponse = array('estatus' => true, 'data' => $arrData);
+						}
+						echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
+					}
+				//}
+				die();
+			}
+
+
+			public function setCategoria_servicios()
+			{
+				if($_POST){
+				//if($_SESSION['permisosMod']['w']){
+					if(empty($_POST['txtNombre_categoria']) || empty($_POST['listEstatus'])  || empty($_POST['txtId_usuario_creacion']))
+						{
+							$arrResponse = array("estatus" => false, "msg" => 'Datos incorrectos.');
+						}else{
+
+						$intIdCategoria_servicios = intval($_POST['idCategoria_servicios']);
+						$strNombre_categoria =  strClean($_POST['txtNombre_categoria']);
+						$intEstatus = intval($_POST['listEstatus']);
+						$strFecha_creacion = strClean($_POST['txtFecha_creacion']);
+						$strFecha_actualizacion = strClean($_POST['txtFecha_actualizacion']);
+						$intId_usuario_creacion = intval($_POST['txtId_usuario_creacion']);
+						$intId_usuario_actualizacion = intval($_POST['txtId_usuario_actualizacion']);
+
+
+						if($intIdCategoria_servicios == 0)
+						{
+							//Crear
+							$request_categoria_servicios = $this->model->insertCategoria_servicios($strNombre_categoria, 
+																								   $intEstatus, 
+																								   $strFecha_creacion, 
+																								   $strFecha_actualizacion, 
+																								   $intId_usuario_creacion, 
+																								   $intId_usuario_actualizacion);
+																								   $option = 1;
+						} 
+
+						if($request_categoria_servicios > 0 )
+						{
+							if($option == 1)
+							{
+								$arrResponse = array('estatus' => true, 'msg' => 'Datos guardados correctamente.');
+							}
+						}else if($request_categoria_servicios == 'exist'){
+							
+							$arrResponse = array('estatus' => false, 'msg' => '¡Atención! La categoría ya existe.');
+						}else{
+							$arrResponse = array("estatus" => false, "msg" => 'No es posible almacenar los datos.');
+						}
+					}
+					echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
+				 //}
+				}
+				die();
+			}
+
+
+			public function setCategoria_servicios_up()
+			{
+				if($_POST)
+				{ //dep ($_POST); die();
+				//if($_SESSION['permisosMod']['w']){
+						if(empty($_POST['txtNombre_categoriaup']) || empty($_POST['listEstatusup']) || empty($_POST['txtId_usuario_actualizacionup']))
+						{
+							$arrResponse = array("estatus" => false, "msg" => 'Datos incorrectos.');
+						}else{
+							$intIdCategoria_servicios = intval($_POST['idCategoria_serviciosup']);
+							$strNombre_categoria =  strClean($_POST['txtNombre_categoriaup']);
+							$intEstatus = intval($_POST['listEstatusup']);
+							$strFecha_actualizacion = strClean($_POST['txtFecha_actualizacionup']);
+							$intId_usuario_actualizacion = intval($_POST['txtId_usuario_actualizacionup']);
+							$request_categoria_servicios = "";
+
+								if($intIdCategoria_servicios <> 0)
+								{
+									$request_categoria_servicios = $this->model->updateCategoria_servicios($intIdCategoria_servicios, 
+																											$strNombre_categoria, 
+																											$intEstatus, 
+																											$strFecha_actualizacion, 
+																											$intId_usuario_actualizacion);
+																											$option = 1;
+								}
+
+								if($request_categoria_servicios > 0 )
+								{
+									if($option == 1)
+									{
+											$arrResponse = array('estatus' => true, 'msg' => 'Datos actualizados correctamente.');
+									}
+								}else{
+										$arrResponse = array("estatus" => false, "msg" => 'No es posible actualizar los datos, probablemente existe un registro con el mismo nombre o presenta algún problema con la red.');
+								}
+							}	
+					echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
+				 //}
+				}
+				die();
+			}
+
+
+			public function delCategoria_servicios()
+			{
+				if($_POST)
+				{
+					//if($_SESSION['permisosMod']['d']){ 
+						$intIdCategoria_servicios = intval($_POST['idCategoria_servicios']);
+						$requestDelete = $this->model->deleteCategoria_servicios($intIdCategoria_servicios);
+						if($requestDelete == 'ok')
+						{
+							$arrResponse = array('estatus' => true, 'msg' => 'Se ha eliminado la categoría correctamente.');
+						}else if($requestDelete == 'exist'){
+							$arrResponse = array('estatus' => false, 'msg' => 'No es posible eliminar una categoría asociado a un servicio activo.');
+						}else{
+							$arrResponse = array('estatus' => false, 'msg' => 'Error al eliminar la categoría.');
+						}
+						echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
+					//}
+				}
+				die();
+			}	
+
+
+
+
+		}
+?>
---
+++ Models/Categoria_serviciosModel.php
@@ -0,0 +1,110 @@
+<?php
+
+class Categoria_serviciosModel extends Mysql
+{
+    public $intIdCategoria_servicios;
+	public $strNombre_categoria;
+	public $intEstatus;
+	public $strFecha_creacion;
+    public $strFecha_actualizacion;
+    public $intId_usuario_creacion;
+    public $intId_usuario_actualizacion;
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+    public function selectCategoria_servicios()
+    {
+        //Extraer las categorias de servicios
+        $sql = "SELECT * FROM t_categoria_servicios WHERE estatus !=0";
+        $request = $this->select_all($sql);
+        return $request;
+    }
+    
+
+    public function selectCategoria_servicio(int $intIdCategoria_servicios)
+    {
+        //Buscar Categoria
+        $this->intIdCategoria_servicios = $intIdCategoria_servicios;
+        $sql = "SELECT * FROM t_categoria_servicios WHERE id = $this->intIdCategoria_servicios";
+        $request = $this->select($sql);
+        return $request;
+    }
+
+
+    public function insertCategoria_servicios(string $nombre_categoria, int $estatus, string $fecha_creacion, string $fecha_actualizacion, int $id_usuario_creacion, int $id_usuario_actualizacion){
+
+        $return = "";
+        $this->strNombre_categoria = $nombre_categoria;
+        $this->intEstatus = $estatus;
+        $this->strFecha_creacion = $fecha_creacion;
+        $this->strFecha_actualizacion = $fecha_actualizacion;
+        $this->intId_usuario_creacion = $id_usuario_creacion;
+        $this->intId_usuario_actualizacion = $id_usuario_actualizacion;
+
+        $sql = "SELECT * FROM t_categoria_servicios WHERE nombre_categoria = '{$this->strNombre_categoria}' ";
+        $request = $this->select_all($sql);
+
+        if(empty($request))
+        {
+            $query_insert = "INSERT INTO t_categoria_servicios(nombre_categoria,estatus,fecha_creacion,fecha_actualizacion,id_usuario_creacion,id_usuario_actualizacion) VALUES(?,?,?,?,?,?)";
+            $arrData = array($this->strNombre_categoria, $this->intEstatus, $this->strFecha_creacion, $this->strFecha_actualizacion, $this->intId_usuario_creacion, $this->intId_usuario_actualizacion );
+            $request_insert = $this->insert($query_insert,$arrData);
+            $return = $request_insert;
+        }else{
+            $return = "exist";
+        }
+        return $return;
+    }	
+
+
+    public function updateCategoria_servicios(int $id, string $nombre_categoria, int $estatus, string $fecha_actualizacion, int $id_usuario_actualizacion){
+
+        $this->intIdCategoria_servicios = $id;
+        $this->strNombre_categoria = $nombre_categoria;
+        $this->intEstatus = $estatus;
+        //$this->strFecha_actualizacion = $fecha_actualizacion;
+        $this->intId_usuario_actualizacion = $id_usuario_actualizacion;
+
+        $sql = "SELECT * FROM t_categoria_servicios WHERE nombre_categoria = '$this->strNombre_categoria' AND id != $this->intIdCategoria_servicios";
+        $request = $this->select_all($sql);
+
+        if(empty($request))
+        {
+            $sql = "UPDATE t_categoria_servicios SET nombre_categoria = ?, estatus = ?, fecha_actualizacion = NOW(), id_usuario_actualizacion = ? WHERE id = $this->intIdCategoria_servicios ";
+            $arrData = array($this->strNombre_categoria, $this->intEstatus, $this->intId_usuario_actualizacion);
+            $request = $this->update($sql,$arrData);
+        }else{
+            $request = "exist";
+        }
+        return $request;			
+    }
+
+
+    public function deleteCategoria_servicios(int $idcategoria_servicios)
+		{
+			$this->intIdCategoria_servicios = $idcategoria_servicios;
+			$sql = "SELECT * FROM t_servicios WHERE id_categoria_servicio = $this->intIdCategoria_servicios";
+			$request = $this->select_all($sql);
+			if(empty($request))
+			{
+				$sql = "UPDATE t_categoria_servicios SET estatus = ? WHERE id = $this->intIdCategoria_servicios";
+				$arrData = array(0);
+				$request = $this->update($sql,$arrData);
+				if($request)
+				{
+					$request = 'ok';	
+				}else{
+					$request = 'error';
+				}
+			}else{
+				$request = 'exist';
+			}
+			return $request;
+		}
+
+
+}
+?>
