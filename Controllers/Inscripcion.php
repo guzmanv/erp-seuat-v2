001fe0dfde44d47040f494c81e889d446e0492ec
JOSEPH1990a <JOSE_SANTIZR1990@HOTMAIL.COM>
2021-12-04 15:08:25 -0600

    agregando sesion en controladores

--- Controllers/Inscripcion.php
+++ Controllers/Inscripcion.php
@@ -2,6 +2,12 @@
     class Inscripcion extends Controllers{
         public function __construct(){
             parent::__construct();
+            session_start();
+		    if(empty($_SESSION['login']))
+		    {
+			    header('Location: '.base_url().'/login');
+			    die();
+		    }
         }
         //Funcion para mostrar Vista(Admision)
         public function admision(){
--- Controllers/Persona.php
+++ Controllers/Persona.php
@@ -2,6 +2,12 @@
     class Persona extends Controllers{
         public function __construct(){
             parent::__construct();
+            session_start();
+		    if(empty($_SESSION['login']))
+		    {
+			    header('Location: '.base_url().'/login');
+			    die();
+		    }
         }
         public function persona(){
             $data['page_id'] = 9;
