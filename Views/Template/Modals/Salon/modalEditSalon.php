64e71e3e91eab095bd725b08c74a81df2aa6bf5f
JOSEPH1990a <JOSE_SANTIZR1990@HOTMAIL.COM>
2021-12-07 10:24:13 -0600

    resolviendo conflicto de Editar  Salones en CES

--- Views/Template/Modals/Salon/modalEditSalon.php
+++ Views/Template/Modals/Salon/modalEditSalon.php
@@ -10,28 +10,11 @@
             </div>
             <div class="modal-body">
                 <div class="card card-secondary">
-<<<<<<< HEAD
-                    <form id="formModalidadEdit" name="formModalidadEdit">
-=======
                     <form id="formSalonEdit" name="formSalonEdit">
->>>>>>> a1086440155120b9fbaf70840dba93c48a670a23
                         <input type="hidden" id="idSalonEdit" name="idSalonEdit" value="">
                         <div class="card-body">
                             <div class="form-group">
                                 <label>Nombre salón</label>
-<<<<<<< HEAD
-                                <input type="text" id="txtNombreEdit" name="txtNombreEdit" class="form-control form-control-sm" placeholder="EJ: Salón 1" name="" maxlength="45" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="">Cantidad máx. alumnos</label>
-                                <input type="number" min="1" pattern="^[0-9]" id="txtCantidadMaxEdit" name="txtCantidadMaxEdit" class="form-control form-control-sm" placeholder="EJ: 10,15,20" required>
-                            </div>
-                            <div class="form-group">
-                                <label>Estatus</label>
-                                <select class="form-control form-control-sm" name="listEstatusEdit" id="listEstatusEdit" required>
-                                    <option value="">Selecciona una opción</option>
-                                    <option value="1">Activado</option>
-=======
                                 <input type="text" id="txtNombreEdit" name="txtNombreEdit" class="form-control form-control-sm" placeholder="EJ: Salón 1" name="" maxlength="15" required>
                             </div>
                             <div class="form-group">
@@ -43,7 +26,6 @@
                                 <select name="slctEstatus" id="slctEstatus" class="form-control form-control-sm" required>
                                     <option value="">Selecciona el estatus de la categoría</option>
                                     <option value="1">Activo</option>
->>>>>>> a1086440155120b9fbaf70840dba93c48a670a23
                                     <option value="2">Inactivo</option>
                                 </select>
                             </div>
