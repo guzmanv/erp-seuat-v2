48628e87793080062bf6c1fd6cf81c92f64b7c6d
JOSEPH1990a <JOSE_SANTIZR1990@HOTMAIL.COM>
2022-01-15 14:59:34 -0600

    a

--- Config/Config.php
+++ Config/Config.php
@@ -1,17 +1,17 @@
 <?php
 
 	//define("BASE_URL", "http://localhost/erp-seuat-v1/");
-	const BASE_URL = "http://localhost:8888/erp-seuat-v2";
+	const BASE_URL = "http://localhost/erp-seuat-v2";
 
 	//Zona horaria
 	date_default_timezone_set('America/Mexico_City');
 
 	//const LIBS = "Libraries/";
 	//const VIEWS = "Views/";
-	const DB_HOST = "localhost:8889";
+	const DB_HOST = "10.10.0.45";
 	const DB_NAME = "erpseuat";
-	const DB_USER = "root";
-	const DB_PASSWORD = "root";
+	const DB_USER = "user_seuat";
+	const DB_PASSWORD = "seuat21";
 	const DB_CHARSET = "utf8";
 
 	//Delimitadores decimal y millar Ej. 27,1985.00
