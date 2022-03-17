0a0727de352c92d92d270a4cf9f9a1de9f50e2e1
JOSEPH1990a <JOSE_SANTIZR1990@HOTMAIL.COM>
2022-03-17 10:59:06 -0600

    agregando menu de servicios

--- Views/Template/nav_admin.php
+++ Views/Template/nav_admin.php
@@ -378,37 +378,17 @@
                   </p>
               </a>
           </li>
+          <li class="nav-header"><h6>Servicios</h6></li>                        
+          <li class="nav-item">
+              <a href="<?php echo BASE_URL ?>/Servicios" class="nav-link">
+                  <i class="ml-3 mr-2" data-feather="dollar-sign"></i>
+                  <p>
+                      Servicios
+                  </p>
+              </a>
+          </li>
 
           <li class="nav-header"><h6>Ingresos y Egresos</h6></li>                        
-          <!-- <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="ml-3 mr-2" data-feather="dollar-sign"></i>
-              <p>
-                Ingresos y Egresos
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                   <i class="ml-3 mr-2" data-feather="circle"></i>
-                  <p>Ingresos</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                   <i class="ml-3 mr-2" data-feather="circle"></i>
-                  <p>Egresos</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                   <i class="ml-3 mr-2" data-feather="circle"></i>
-                  <p>Consultas</p>
-                </a>
-              </li>
-            </ul>
-          </li> -->
           <li class="nav-item">
               <a href="<?php echo BASE_URL ?>/Ingresos" class="nav-link">
                   <i class="ml-3 mr-2" data-feather="dollar-sign"></i>
